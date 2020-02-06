96eaffd83ac9a1131796ceda91d78e0993d483f9
Alexsey Vishnyakov <pms146@yandex.ru>
2020-02-06 17:38:43 +0300

    final 24

--- src/components/product-block/product-block.vue
+++ src/components/product-block/product-block.vue
@@ -13,6 +13,8 @@
         <img src="../../assets/images/H7p.png" alt="H7p">
         <img src="../../assets/images/H11p.png" alt="H11p">
         <img src="../../assets/images/Hb4p.png" alt="Hb4p">
+        <img src="../../assets/images/F1H8.png" alt="H4">
+
       </div>
       <div class="market" id="markets">
         <div class="title">где купить <img src="../../assets/images/arrow_down.png" alt=""></div>
@@ -24,6 +26,14 @@
           <a href="https://autopiter.ru/" target="_blank"><img src="../../assets/images/avtopiter.png" alt=""></a>
           <a href="https://svetodiod96.ru/catalog/lampy-fukurou/" target="_blank"><img src="../../assets/images/svetodoid96.png" alt=""></a>
           <a href="http://avtokomfort33.ru/index.php?route=product/category&path=87_254" target="_blank"><img src="../../assets/images/avto-comfort.png" alt=""></a>
+          <a href="https://www.wildberries.ru/brands/fukurou" target="_blank"><img src="../../assets/images/wildberries.png" alt=""></a>
+
+          <a href="https://emex.ru/" target="_blank"><img src="../../assets/images/emex.png" alt="emex"></a>
+          <a href="https://ixora-auto.ru/" target="_blank"><img src="../../assets/images/ixora.png" alt="ixora"></a>
+          <a href="https://www.autodoc.ru/" target="_blank"><img src="../../assets/images/autodoc-ru.png" alt="autodoc"></a>
+          <a href="https://xn--90acjcjbhmu6bn.xn--p1ai/catalog/galogennye_avtomobilnye_lampy/filter/proizvoditel-is-5e3cf814-1de8-11ea-9493-181dead4976a/apply/" target="_blank">
+            <img src="../../assets/images/light.png" alt="autodoc">
+          </a>
         </div>
       </div>
     </div>
