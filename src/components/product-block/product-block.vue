a24893218def463d7ed29b4f7287edc74e980719
Alexsey Vishnyakov <pms146@yandex.ru>
2020-02-07 11:12:09 +0300

    final 25

--- src/components/product-block/product-block.vue
+++ src/components/product-block/product-block.vue
@@ -20,7 +20,7 @@
         <div class="title">где купить <img src="../../assets/images/arrow_down.png" alt=""></div>
 
         <div class="markets" >
-          <a href="https://www.ulmart.ru/catalog/lampy_i_oborudovanie_golovnogo_osvesenia_1/brand_fukurou" target="_blank"><img src="../../assets/images/ulmart.png" alt=""></a>
+<!--          <a href="https://www.ulmart.ru/catalog/lampy_i_oborudovanie_golovnogo_osvesenia_1/brand_fukurou" target="_blank"><img src="../../assets/images/ulmart.png" alt=""></a>-->
           <a href="https://www.ozon.ru/seller/2483/products/" target="_blank"><img src="../../assets/images/ozon.png" alt=""></a>
           <a href="https://avtobridge.ru/lamps?brand[]=Fukurou&price_mode=price_only&initial_filter=brand%5B%5D" target="_blank"><img src="../../assets/images/avtobridge.png" alt=""></a>
           <a href="https://autopiter.ru/" target="_blank"><img src="../../assets/images/avtopiter.png" alt=""></a>
@@ -31,6 +31,7 @@
           <a href="https://emex.ru/" target="_blank"><img src="../../assets/images/emex.png" alt="emex"></a>
           <a href="https://ixora-auto.ru/" target="_blank"><img src="../../assets/images/ixora.png" alt="ixora"></a>
           <a href="https://www.autodoc.ru/" target="_blank"><img src="../../assets/images/autodoc-ru.png" alt="autodoc"></a>
+          <a href="https://www.autodoc.ru/" target="_blank"><img src="../../assets/images/exist.png" alt="exist"></a>
           <a href="https://xn--90acjcjbhmu6bn.xn--p1ai/catalog/galogennye_avtomobilnye_lampy/filter/proizvoditel-is-5e3cf814-1de8-11ea-9493-181dead4976a/apply/" target="_blank">
             <img src="../../assets/images/light.png" alt="autodoc">
           </a>
