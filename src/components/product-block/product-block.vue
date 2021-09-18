e8b58ffe49e71985daffa5e07bee7a7cf13da05b
Alexsey Vishnyakov <pms146@yandex.ru>
2021-09-18 16:04:06 +0300

    правки ссылок

--- src/components/product-block/product-block.vue
+++ src/components/product-block/product-block.vue
@@ -32,18 +32,18 @@
 
         <div class="markets" >
 <!--          <a href="https://www.ulmart.ru/catalog/lampy_i_oborudovanie_golovnogo_osvesenia_1/brand_fukurou" target="_blank"><img src="../../assets/images/ulmart.png" alt=""></a>-->
-          <a href="https://www.ozon.ru/seller/2483/products/" target="_blank"><img src="../../assets/images/ozon.png" alt=""></a>
+          <a href="https://www.ozon.ru/seller/avtobridge-33410/products/" target="_blank"><img src="../../assets/images/ozon.png" alt=""></a>
           <a href="https://avtobridge.ru/lamps?brand[]=Fukurou&price_mode=price_only&initial_filter=brand%5B%5D" target="_blank"><img src="../../assets/images/avtobridge.png" alt=""></a>
           <a href="https://autopiter.ru/" target="_blank"><img src="../../assets/images/avtopiter.png" alt=""></a>
           <a href="https://svetodiod96.ru/catalog/lampy-fukurou/" target="_blank"><img src="../../assets/images/svetodoid96.png" alt=""></a>
           <a href="https://www.wildberries.ru/brands/fukurou" target="_blank"><img src="../../assets/images/wildberries.png" alt=""></a>
-          <a href="https://www.vseinstrumenti.ru/" target="_blank"><img src="../../assets/images/vse-instruments.png" alt=""></a>
+          <a href="https://vseinstrumenti.ru/fukurou/" target="_blank"><img src="../../assets/images/vse-instruments.png" alt=""></a>
 
           <a href="https://emex.ru/" target="_blank"><img src="../../assets/images/emex.png" alt="emex"></a>
           <a href="https://ixora-auto.ru/" target="_blank"><img src="../../assets/images/ixora.png" alt="ixora"></a>
           <a href="https://www.autodoc.ru/" target="_blank"><img src="../../assets/images/autodoc-ru.png" alt="autodoc"></a>
           <a href="https://www.exist.ru/" target="_blank"><img src="../../assets/images/exist.png" alt="exist"></a>
-          <a href="https://xn--90acjcjbhmu6bn.xn--p1ai/catalog/galogennye_avtomobilnye_lampy/filter/proizvoditel-is-5e3cf814-1de8-11ea-9493-181dead4976a/apply/" target="_blank">
+          <a href="https://xn--90acjcjbhmu6bn.xn--p1ai/catalog/avtolampy_galogennye_/filter/proizvoditel-is-5e3cf814-1de8-11ea-9493-181dead4976a/apply/" target="_blank">
             <img src="../../assets/images/light.png" alt="autodoc">
           </a>
           <a href="http://avtokomfort33.ru/index.php?route=product/category&path=87_254" target="_blank"><img src="../../assets/images/avto-comfort.png" alt=""></a>
